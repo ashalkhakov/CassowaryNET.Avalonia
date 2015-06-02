0b56b7dbd5c4a9db984dd4b909e1d1e8a49620f1
Simon Cowen <simoncowen88@gmail.com>
2015-06-02 14:22:58 +0100

    LayoutNET.Tests added

---
+++ LayoutNET.Tests/_Class1Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace LayoutNET.Tests
+{
+    public class Class1 { }
+
+    [TestFixture]
+    public class Class1Tests
+    {
+        #region Helpers
+
+        public abstract class TestBase
+        {
+            [SetUp]
+            public virtual void SetUp()
+            {
+            }
+
+            [TearDown]
+            public virtual void TearDown()
+            {
+            }
+
+            internal Class1 GetTarget()
+            {
+                return new Class1();
+            }
+        }
+
+        #endregion
+
+        [TestFixture]
+        public class ConstructorTests //: TestBase
+        {
+            [Test]
+            public void can_be_created()
+            {
+                var target = new Class1();
+                Assert.Pass();
+            }
+        }
+
+        [TestFixture]
+        public class Method1Tests : TestBase
+        {
+            [Test]
+            public void when_foo_then_bar()
+            {
+            }
+        }
+    }
+}
