693b2f8a610b829225491b34220587bbbae7518e
Simon Cowen <simoncowen88@gmail.com>
2015-06-02 14:00:23 +0100

    Renaming of projects and cleanup from project upgrades

--- CassowaryNET.Tests/Properties/AssemblyInfo.cs
+++ CassowaryNET.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Cassowary")]
+[assembly: AssemblyTitle("CassowaryNET.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cassowary")]
+[assembly: AssemblyProduct("CassowaryNET.Tests")]
 [assembly: AssemblyCopyright("Copyright © Simon Cowen 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,12 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("e53ef3b7-229a-4b14-8086-c0da77f1883d")]
+[assembly: Guid("4704cf20-713d-4cf3-9d8f-9d060b194ec2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,12 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// For tests
-
-[assembly: InternalsVisibleTo("Cassowary.Tests")]
-// For Moq
-
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- CassowaryNET/Properties/AssemblyInfo.cs
+++ CassowaryNET/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Cassowary.Tests")]
+[assembly: AssemblyTitle("CassowaryNET")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cassowary.Tests")]
+[assembly: AssemblyProduct("CassowaryNET")]
 [assembly: AssemblyCopyright("Copyright © Simon Cowen 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,12 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("4704cf20-713d-4cf3-9d8f-9d060b194ec2")]
+[assembly: Guid("c5d703fa-3e32-4bf1-9fd5-2e0555419c03")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,6 +32,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// For tests
+[assembly: InternalsVisibleTo("CassowaryNET.Tests")]
+// For Moq
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
